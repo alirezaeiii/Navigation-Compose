1d3e87150cb781d63193f2eaf7cf492b96eb7d4d
Ali Rezaei <ali.rezaei09@gmail.com>
2022-02-14 15:46:30 +0330

    Cleanup

--- app/src/main/java/com/android/sample/app/di/NetworkModule.kt
+++ app/src/main/java/com/android/sample/app/di/NetworkModule.kt
@@ -8,10 +8,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import timber.log.Timber
 import javax.inject.Singleton
 
 /**
